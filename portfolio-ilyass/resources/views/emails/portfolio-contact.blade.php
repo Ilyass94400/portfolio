27579c62a1568d06d22bfc40746ffdd7f4dd9ca1
Ilyass <ilyass.gourmat.sio@gmail.com>
2026-03-29 21:05:52 +0200

    dossier supprimé 1

--- portfolio-ilyass/resources/views/emails/portfolio-contact.blade.php
+++
@@ -1,9 +0,0 @@
-<h2>Nouveau message de ton Portfolio</h2>
-
-<p><strong>Expéditeur :</strong> {{ $data['name'] }}</p>
-<p><strong>Email :</strong> {{ $data['email'] }}</p>
-
-<hr>
-
-<p><strong>Message :</strong></p>
-<p>{{ $data['message'] }}</p>
